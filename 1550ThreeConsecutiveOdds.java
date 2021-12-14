26889ecc36a5d1e85a95b8d6803540c7f40d9b44
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2021-12-13 22:19:45 -0600

    Create 1550ThreeConsecutiveOdds.java

---
+++ 1550ThreeConsecutiveOdds.java
@@ -0,0 +1,17 @@
+//Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr)
+    {
+        for(int i = 2; i < arr.length; i++)
+        {
+            if((arr[i] % 2 != 0) && (arr[i-1] % 2 != 0) && (arr[i-2] % 2 != 0))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+}
