20381e9af7a097e3486502c251491674b957e48c
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2022-01-06 15:57:48 -0800

    Create 35SearchInsertPosition.cpp

---
+++ 35SearchInsertPosition.cpp
@@ -0,0 +1,21 @@
+// CPP VERSION -- already completed program in python and java earlier
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == target)
+            {
+                return i;
+            }
+            else if(nums[i] > target)
+            {
+                return i;
+            }
+        }
+        
+        return nums.size();
+    }
+};
