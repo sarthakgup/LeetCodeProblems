1785fe07e6e3045184904df3bbfd70e9072806d0
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2022-01-06 15:53:27 -0800

    Update 35SearchInsertPosition.py

--- 35SearchInsertPosition.py
+++ 35SearchInsertPosition.py
@@ -1,7 +1,7 @@
 # Given a sorted array of distinct integers and a target value, return the index if the target is found.
 # If not, return the index where it would be if it were inserted in order.
 
-# 56 ms runtime (did a perfect time solution in Java
+# 56 ms runtime (did a perfect time solution in Java)
 
 # You must write an algorithm with O(log n) runtime complexity.
 
